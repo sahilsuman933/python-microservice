41b58d2d277a7e93767dd7e0fcf076f276b1800b
Sahil Suman <sahilsuman933@gmail.com>
2023-11-08 19:39:45 +0530

    added search_wiki function
    Signed-off-by: Sahil Suman <sahilsuman933@gmail.com>

--- mylib/logic.py
+++ mylib/logic.py
@@ -1,4 +1,4 @@
-from wikipedia import wikipedia
+import wikipedia
 
 
 def wiki(name="War Goddess", length=1):
@@ -6,3 +6,18 @@ def wiki(name="War Goddess", length=1):
 
     my_wiki = wikipedia.summary(name, length)
     return my_wiki
+
+
+def search_wiki(name):
+    """Search Wikipedia for Names"""
+
+    results = wikipedia.search(name)
+    return results
+
+
+# def phrase(name):
+#     """Returns phrases from wikipedia"""
+#
+#     page = wiki(name)
+#     blob = TextBlob(page)
+#     return blob.noun_phrases
